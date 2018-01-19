4c2ad3adc9e754b23528b7917cca6aa5a10cf8ba
Ian Lumsden <lumsden.ian@gmail.com>
2018-01-19 12:15:32 -0500

    Removes basic_driver.py from the python directory. Also, removes all __init__.py file to fix issues for Python 3.3 and later.

--- python/basic_driver.py
+++
@@ -1,7 +0,0 @@
-from components import Block, Cone, Cylinder, Sphere, Torus
-from operations import Binary, Difference, Dilation, Intersection
-from operations import Reflection, Rotation, Transformation, Translation, Union
-from Parser import Parser
-
-parser = Parser("test4.xml")
-parser.createSCAD()
