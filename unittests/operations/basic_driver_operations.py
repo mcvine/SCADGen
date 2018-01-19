2b7f97c6b75acbf7225e2c8edfe6c38a655c86f0
Ian Lumsden <lumsden.ian@gmail.com>
2018-01-19 12:13:10 -0500

    Adds a new directory for 'Unit Tests' and the actual tests.

---
+++ unittests/operations/basic_driver_operations.py
@@ -0,0 +1,11 @@
+import sys
+import os
+sys.path.append(os.path.abspath("../../python"))
+
+import glob
+from Parser import Parser
+
+filelist = glob.glob(os.path.abspath(".")+"/*.xml")
+for fname in filelist:
+    parser = Parser(fname)
+    parser.createSCAD()
