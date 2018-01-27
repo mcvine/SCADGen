5bc3e10a72c8f3eefb32cf26d393c536f46c0478
Ian Lumsden <lumsden.ian@gmail.com>
2018-01-27 14:25:36 -0500

    Updates the import path in the drivers to ensure they work. Additionally, fixed a small problem with the commenting in Parser.py

--- SCADGen/python/Parser.py
+++ SCADGen/python/Parser.py
@@ -19,10 +19,12 @@ import xml.etree.ElementTree as ET
 class Parser:
 
     def __init__(self, xml_file):
-        """This constructor creates an XML ElementTree object
-           from the xml file passed as a parameter. Then, after
-           setting the other members to default values, it calls
-           the parse() function."""
+        """
+        This constructor creates an XML ElementTree object
+        from the xml file passed as a parameter. Then, after
+        setting the other members to default values, it calls
+        the parse() function.
+        """
         self.xmltree = ET.parse(xml_file)
         self.rootelems = []
         self.containsTorus = False
@@ -31,38 +33,41 @@ class Parser:
         return
 
     def parse(self):
-        """This function calls the getRootElem function
-           on the top-level elements to generate the Python
-           object structure that will be used to create the
-           OpenSCAD code."""
+        """
+        This function calls the getRootElem function
+        on the top-level elements to generate the Python
+        object structure that will be used to create the
+        OpenSCAD code.
+        """
         root = self.xmltree.getroot()
         for child in root:
             self.rootelems.append(self.getRootElem(child))
 
     def getRootElem(self, elem):
-        """Using helper functions, this function recursively
-           generates the Python object structure of a root element
-           and its subelements."""
-        assert(elem is ET.Element, "Error: {0!s} is not an XML Element".format(elem))
-        """If the element is a component, it cannot have any children,
-           so the Python object for it is returned."""
+        """
+        Using helper functions, this function recursively
+        generates the Python object structure of a root element
+        and its subelements.
+        """
+        # If the element is a component, it cannot have any children,
+        # so the Python object for it is returned."""
         if self.isComp(elem):
             return self.makeComp(elem)
-        """If the element is a binary operation, it is created, and its
-           children in the Python object are set by recursively calling
-           this function. The operation is then returned."""
+        # If the element is a binary operation, it is created, and its
+        # children in the Python object are set by recursively calling
+        # this function. The operation is then returned.
         elif self.isBinary(elem):
             children = list(elem)
             bin_op = self.makeBinary(elem)
             bin_op.comp1 = self.getRootElem(children[0])
             bin_op.comp2 = self.getRootElem(children[1])
             return bin_op
-        """If the element is an unary operation, the code determines
-           which of the element's children is a component or operation
-           and which are attributes. After determining this, the Python
-           object for the unary operation is created, and its body member
-           is set by recursively calling this function. Finally, the
-           Python object for the unary operation is returned."""
+        # If the element is an unary operation, the code determines
+        # which of the element's children is a component or operation
+        # and which are attributes. After determining this, the Python
+        # object for the unary operation is created, and its body member
+        # is set by recursively calling this function. Finally, the
+        # Python object for the unary operation is returned.
         else:
             comp1 = None
             attrs = []
@@ -76,12 +81,14 @@ class Parser:
             return un_op
         
     def isComp(self, elem):
-        """This function returns True if the element's tag identifies
-           it as such. If the element has the tag "generalized-cone",
-           a NotImplementedError is currently returned because it is not
-           yet implemented in this repository. This will be removed later.
-           Otherwise, the element is not a component, and the function
-           returns False."""
+        """
+        This function returns True if the element's tag identifies
+        it as such. If the element has the tag "generalized-cone",
+        a NotImplementedError is currently returned because it is not
+        yet implemented in this repository. This will be removed later.
+        Otherwise, the element is not a component, and the function
+        returns False.
+        """
         tag = elem.tag
         if tag == "block" or tag == "cone" or tag == "cylinder" or tag == "sphere" or tag == "torus":
             return True
@@ -91,11 +98,13 @@ class Parser:
             return False
 
     def makeComp(self, elem):
-        """This function uses the element's tag and its
-           ElementTree object to create the correct Python
-           object, which is then returned. If the element trying
-           to be created is not (yet) implemented in this repository,
-           a NotImplementedError is raised."""
+        """
+        This function uses the element's tag and its
+        ElementTree object to create the correct Python
+        object, which is then returned. If the element trying
+        to be created is not (yet) implemented in this repository,
+        a NotImplementedError is raised.
+        """
         tag = elem.tag
         if tag == "block":
             return Block(elem)
@@ -114,9 +123,11 @@ class Parser:
             raise NotImplementedError("{0!s} is not implemented".format(tag))
 
     def isBinary(self, elem):
-        """If the element's tag identifies it as a binary
-           operation, this function returns True. Otherwise,
-           it returns False."""
+        """
+        If the element's tag identifies it as a binary
+        operation, this function returns True. Otherwise,
+        it returns False.
+        """
         tag = elem.tag
         if tag == "difference" or tag == "intersection" or tag == "union":
             return True
@@ -124,10 +135,12 @@ class Parser:
             return False
 
     def makeBinary(self, elem):
-        """Using the element's tag, this function creates the
-           correct basic Python object, which is then returned.
-           If the element is not (yet) implemented in this
-           repository, a NotImplementedError is raised."""
+        """
+        Using the element's tag, this function creates the
+        correct basic Python object, which is then returned.
+        If the element is not (yet) implemented in this
+        repository, a NotImplementedError is raised.
+        """
         tag = elem.tag
         if tag == "difference":
             return Difference()
@@ -139,12 +152,14 @@ class Parser:
             raise NotImplementedError("{0!s} is not implemented".format(tag))
 
     def isUnary(self, elem):
-        """If the element's tag identifies it as an unary
-           operation, this function returns True. Otherwise,
-           it returns False. If the element is a reversal,
-           a NotImplementedError is raised, as the reversal operation
-           has not yet been implemented in this repository. This will
-           be removed later."""
+        """
+        If the element's tag identifies it as an unary
+        operation, this function returns True. Otherwise,
+        it returns False. If the element is a reversal,
+        a NotImplementedError is raised, as the reversal operation
+        has not yet been implemented in this repository. This will
+        be removed later.
+        """
         tag = elem.tag
         if tag == "dilation" or tag == "reflection" or tag == "rotation" or tag == "translation":
             return True
@@ -154,14 +169,16 @@ class Parser:
             return False
 
     def makeUnary(self, elem, attrs):
-        """Using the element's tag and the attributes determined
-           in the getRootElems() function, this function creates the
-           correct basic Python object, which is then returned.
-           If the element is not (yet) implemented in this
-           repository, a NotImplementedError is raised. The extra
-           code in this function compared to the other "make" functions
-           is used to pass the correct attributes into the constructor
-           for the Python object being made."""
+        """
+        Using the element's tag and the attributes determined
+        in the getRootElems() function, this function creates the
+        correct basic Python object, which is then returned.
+        If the element is not (yet) implemented in this
+        repository, a NotImplementedError is raised. The extra
+        code in this function compared to the other "make" functions
+        is used to pass the correct attributes into the constructor
+        for the Python object being made.
+        """
         tag = elem.tag
         assert(len(attrs) > 0)
         if tag == "dilation":
@@ -206,15 +223,17 @@ class Parser:
 }\n\n"""
 
     def createSCAD(self):
-        """This function creates the OpenSCAD file from a Parser object."""
+        """
+        This function creates the OpenSCAD file from a Parser object.
+        """
         fname = self.filename[:-3] + "scad"
         scadfile = open(fname, "w+")
-        """If the XML file contains a torus, the torus module is
-           added to the beginning of the OpenSCAD file."""
+        # If the XML file contains a torus, the torus module is
+        # added to the beginning of the OpenSCAD file.
         if self.containsTorus:
             scadfile.write(self.printTorusModule())
-        """This for loop causes any root elements that do not have
-           children to not be printed to the OpenSCAD file."""
+        # This for loop causes any root elements that do not have
+        # children to not be printed to the OpenSCAD file.
         for elem in self.rootelems: 
             if elem.isComp():
                 continue
--- tests/unittests/components/basic_driver_components.py
+++ tests/unittests/components/basic_driver_components.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import
 
 import sys
 import os
-sys.path.append(os.path.abspath("../../../python"))
+sys.path.append(os.path.abspath("../../../SCADGen/python"))
 
 import glob
 from Parser import Parser
