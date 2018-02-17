1fe3eef0710f02bb877615621a064732aa97fff8
Ian Lumsden <lumsden.ian@gmail.com>
2018-02-17 16:08:28 -0500

    Fixes a relative import bug, and implements an eq operator for Binary operations.

--- SCADGen/python/operations/Binary.py
+++ SCADGen/python/operations/Binary.py
@@ -7,3 +7,11 @@ class Binary:
 
     def isComp(self):
         return False
+
+    def __eq__(self, rhs):
+        if type(self) != type(rhs):
+            return False
+        elif self.comp1 != rhs.comp1 or self.comp2 != rhs.comp2:
+            return False
+        else:
+            return True
