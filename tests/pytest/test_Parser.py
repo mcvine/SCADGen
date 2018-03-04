ca613d880942801cb86fd5ac815ae4255d0558f2
Ian Lumsden <lumsden.ian@gmail.com>
2018-03-04 13:08:46 -0500

    Adds more tests for Parser.py and a new empty test XML

--- tests/pytest/test_Parser.py
+++ tests/pytest/test_Parser.py
@@ -9,6 +9,7 @@ import xml.etree.ElementTree as et
 from Parser import Parser
 import components
 import operations
+import pytest
 
 def test_parsercore():
     fname = os.path.abspath("./test_xmls/parser_test.xml")
@@ -23,6 +24,67 @@ def test_parsercore():
     sol.comp2 = op
     p = Parser(fname)
     test = p.rootelems[0]
+    assert(test == sol)
+
+def test_isCompTrue():
+    elem = et.Element("cylinder", {"radius" : "1.5", "height" : "10" })
+    fname = os.path.abspath("./test_xmls/empty.xml")
+    p = Parser(fname)
+    assert(p.isComp(elem) == True)
+
+def test_isCompFalse():
+    elem = et.Element("difference")
+    fname = os.path.abspath("./test_xmls/empty.xml")
+    p = Parser(fname)
+    assert(p.isComp(elem) == False)
+
+def test_isBinaryTrue():
+    elem = et.Element("difference")
+    fname = os.path.abspath("./test_xmls/empty.xml")
+    p = Parser(fname)
+    assert(p.isComp(elem) == True)
+
+def test_isBinaryFalse():
+    elem = et.Element("cylinder", {"radius" : "1.5", "height" : "10" })
+    fname = os.path.abspath("./test_xmls/empty.xml")
+    p = Parser(fname)
+    assert(p.isComp(elem) == False)
+
+def test_isUnaryTrue():
+    elem = et.Element("dilation")
+    fname = os.path.abspath("./test_xmls/empty.xml")
+    p = Parser(fname)
+    assert(p.isComp(elem) == True)
+
+def test_isUnaryFalse():
+    elem = et.Element("cylinder", {"radius" : "1.5", "height" : "10" })
+    fname = os.path.abspath("./test_xmls/empty.xml")
+    p = Parser(fname)
+    assert(p.isComp(elem) == False)
+
+def test_makeComp():
+    elem = et.Element("cylinder", {"radius" : "1.5", "height" : "10" })
+    fname = os.path.abspath("./test_xmls/empty.xml")
+    p = Parser(fname)
+    test = p.makeComp(elem)
+    sol = components.Cylinder(elem)
+    assert(test == sol)
+
+def test_makeBinary():
+    elem = et.Element("difference")
+    fname = os.path.abspath("./test_xmls/empty.xml")
+    p = Parser(fname)
+    test = p.makeBinary(elem)
+    sol = operations.Difference()
+    assert(test == sol)
+
+def test_makeUnary():
+    elem = et.Element("dilation")
+    attrs = [et.Element("scale", text="5")]
+    fname = os.path.abspath("./test_xmls/empty.xml")
+    p = Parser(fname)
+    test = p.makeUnary(elem, attrs)
+    sol = operations.Dilation(float(5))
     assert(test == sol)
 
 def test_generateSCAD():    
