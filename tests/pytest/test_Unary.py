67af8993504602d2381a3679067ddbdfd8732806
Ian Lumsden <lumsden.ian@gmail.com>
2018-03-04 14:43:20 -0500

    Updates the tests for unary operations.

--- tests/pytest/test_Unary.py
+++ tests/pytest/test_Unary.py
@@ -9,38 +9,56 @@ from Parser import Parser
 import components
 import operations
 
-def test_dilation():
-    fname = os.path.abspath("../unittests/operations/Dilation.xml")
-    p = Parser(fname)
-    test = p.rootelems[0]
+def test_dilation_scale():
     elem1 = et.Element("block", { "diagonal" : "(1, 1, 1)" })
-    sol = operations.Dilation(5)
-    sol.body = components.Block(elem1)
-    assert(test == sol)
+    op = operations.Dilation(5)
+    op.body = components.Block(elem1)
+    assert(op.scale == 5)
 
-def test_reflection():
-    fname = os.path.abspath("../unittests/operations/Reflection.xml")
-    p = Parser(fname)
-    test = p.rootelems[0]
+def test_dilation_body():
+    elem1 = et.Element("block", { "diagonal" : "(1, 1, 1)" })
+    op = operations.Dilation(5)
+    op.body = components.Block(elem1)
+    assert(op.body.x == 1 and op.body.y == 1 and op.body.z == 1)
+
+def test_reflection_vector():
     elem1 = et.Element("cylinder", { "height" : "5", "radius" : "2.5" })
-    sol = operations.Reflection("(1, 1, 1)")
-    sol.body = components.Cylinder(elem1)
-    assert(test == sol)
+    op = operations.Reflection("(1, 1, 1)")
+    op.body = components.Cylinder(elem1)
+    assert(op.vector == [1, 1, 1])
 
-def test_rotation():
-    fname = os.path.abspath("../unittests/operations/Rotation.xml")
-    p = Parser(fname)
-    test = p.rootelems[0]
+def test_reflection_body():
+    elem1 = et.Element("cylinder", { "height" : "5", "radius" : "2.5" })
+    op = operations.Reflection("(1, 1, 1)")
+    op.body = components.Cylinder(elem1)
+    assert(op.body.height == 5 and op.body.radius == 2.5)
+
+def test_rotation_angle():
     elem1 = et.Element("block", { "diagonal" : "(5, 5, 5)" })
-    sol = operations.Rotation(45, "(0, 0, 1)")
-    sol.body = components.Block(elem1)
-    assert(test == sol)
+    op = operations.Rotation(45, "(0, 0, 1)")
+    op.body = components.Block(elem1)
+    assert(op.angle == 45)
 
-def test_translation():
-    fname = os.path.abspath("../unittests/operations/Translation.xml")
-    p = Parser(fname)
-    test = p.rootelems[0]
+def test_rotation_vector():
+    elem1 = et.Element("block", { "diagonal" : "(5, 5, 5)" })
+    op = operations.Rotation(45, "(0, 0, 1)")
+    op.body = components.Block(elem1)
+    assert(op.vector == [0, 0, 1])
+
+def test_rotation_body():
+    elem1 = et.Element("block", { "diagonal" : "(5, 5, 5)" })
+    op = operations.Rotation(45, "(0, 0, 1)")
+    op.body = components.Block(elem1)
+    assert(op.body.x == 5 and op.body.y == 5 and op.body.z == 5)
+
+def test_translation_vector():
     elem1 = et.Element("sphere", { "radius" : "5" })
-    sol = operations.Translation("(10, 0, 0)")
-    sol.body = components.Sphere(elem1)
-    assert(test == sol)
+    op = operations.Translation("(10, 0, 0)")
+    op.body = components.Sphere(elem1)
+    assert(op.vector == [10, 0, 0])
+
+def test_translation_body():
+    elem1 = et.Element("sphere", { "radius" : "5" })
+    op = operations.Translation("(10, 0, 0)")
+    op.body = components.Sphere(elem1)
+    assert(op.body.radius == 5)
