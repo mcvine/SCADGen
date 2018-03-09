ef287eab60066b5d2d3bb33825a40d9ec885c29a
Ian Lumsden <lumsden.ian@gmail.com>
2018-03-09 17:03:22 -0500

    Fixes the __init__ so that the components can be correctly imported in Parser.

--- SCADGen/python/components/__init__.py
+++ SCADGen/python/components/__init__.py
@@ -1,8 +1,8 @@
 from __future__ import absolute_import
 
-from Block import Block
-from Cone import Cone
-from Cylinder import Cylinder
-from Pyramid import Pyramid
-from Sphere import Sphere
-from Torus import Torus
+from .Block import Block
+from .Cone import Cone
+from .Cylinder import Cylinder
+from .Pyramid import Pyramid
+from .Sphere import Sphere
+from .Torus import Torus
