84211a8fc0f15b4fb778be35c3f99e3939e183c9
Ian Lumsden <lumsden.ian@gmail.com>
2018-03-09 17:12:00 -0500

    Fixes a small formatting error in the str function.

--- SCADGen/python/components/Pyramid.py
+++ SCADGen/python/components/Pyramid.py
@@ -24,7 +24,7 @@ class Pyramid(Component):
         Cone object.
         """
         return """polyhedron(
-    points=[ [{0!s},{1!s},-{2!s}], [{0!s},-{1!s},{-2!s}],
+    points=[ [{0!s},{1!s},-{2!s}], [{0!s},-{1!s},-{2!s}],
              [-{0!s},-{1!s},-{2!s}], [-{0!s},{1!s},-{2!s}], [0,0,0] ],
     faces=[ [0,1,4], [1,2,4], [2,3,4], [3,0,4], [1,0,3], [2,1,3] ]
 );""".format((self.edgeX/2), (self.edgeY/2), self.height)
