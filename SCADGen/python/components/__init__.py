0e0ec5a691964802f3dbf92e070d152ef1ba711f
Ian Lumsden <lumsden.ian@gmail.com>
2018-03-09 16:58:15 -0500

    Updates the necessary files to allow the Parser to work with Pyramid objects.

--- SCADGen/python/components/__init__.py
+++ SCADGen/python/components/__init__.py
@@ -3,5 +3,6 @@ from __future__ import absolute_import
 from Block import Block
 from Cone import Cone
 from Cylinder import Cylinder
+from Pyramid import Pyramid
 from Sphere import Sphere
 from Torus import Torus
