e8a5c1af4020c083cbcf836306cabe3364c092d6
Jiao Lin <linjiao@ornl.gov>
2018-03-22 16:39:03 -0400

    allow test one feature in tests/comp_tests/basic_driver.py

--- tests/comp_tests/basic_driver.py
+++ tests/comp_tests/basic_driver.py
@@ -1,13 +1,20 @@
+"""
+$ python basic_driver.py             # run all tests
+$ python basic_driver.py block       # run block tests
+"""
+
 from __future__ import absolute_import
 
 import sys
+feature = sys.argv[1] if len(sys.argv)>1 else ''
+
 import os
 sys.path.append(os.path.abspath("../../SCADGen/python"))
 
 import glob
 from Parser import Parser
 
-filelist = glob.glob(os.path.abspath(".")+"/*.xml")
+filelist = glob.glob(os.path.abspath(".")+"/%s*.xml" % feature)
 for fname in filelist:
     parser = Parser(fname)
     parser.createSCAD()
