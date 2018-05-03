8c7e25ccb958813711e278478f4c75a5a2677340
Jiao Lin <linjiao@ornl.gov>
2018-05-03 14:25:23 -0400

    added _version.py and setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,27 @@
-import setuptools import setup
+import os
+from setuptools import setup, find_packages
 
+here = os.path.dirname(__file__)
+version_ns = {}
+with open(os.path.join(here, 'SCADGen', '_version.py')) as f:
+    exec(f.read(), {}, version_ns)
+
+# define distribution
 setup(
-    name="SCADGen",
-    version="0.1a",
-    description="Tools for converting MCViNE XMLs into OpenSCAD code",
+    name = "SCADGen",
+    version = version_ns['__version__'],
+    packages = find_packages(".", exclude=['tests', 'travis']),
+    package_dir = {'': "."},
+    data_files = [],
+    test_suite = 'tests',
+    install_requires = [
+    ],
+    dependency_links = [
+    ],
+    author = "MCViNE team",
+    description="Tools for converting MCViNE sample assembly XMLs into OpenSCAD code",
+    license = 'BSD',
+    keywords = "instrument, neutron, CAD",
     url="https://github.com/mcvine/SCADGen/",
-    packages=[],
+    # download_url = '',
 )
